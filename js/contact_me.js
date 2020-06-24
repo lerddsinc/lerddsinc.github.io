c718bdd7ea2c6fee47a87650fc37304910d44863
lerddsinc <61530971+lerddsinc@users.noreply.github.com>
2020-06-23 20:51:18 -0500

    Update contact_me.js
    Change

--- js/contact_me.js
+++ js/contact_me.js
@@ -42,8 +42,8 @@ $(function() {
           $('#contactForm').trigger("reset");
         },
         error: function() {
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
+          // Fal message
+          $('#succiess').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
